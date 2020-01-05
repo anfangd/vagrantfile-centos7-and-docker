069ce5b249e360e8d4d51ad043cd9b76620e30fc
anfangd <>
2020-01-05 23:52:46 +0900

    [add] install AWS SAM CLI

--- provision.sh
+++ provision.sh
@@ -70,3 +70,7 @@ su -l vagrant -c "git clone https://github.com/aws/aws-elastic-beanstalk-cli-set
 su -l vagrant -c "./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer"
 echo 'export PATH="/home/vagrant/.ebcli-virtual-env/executables:$PATH"' >> /home/vagrant/.bash_profile && source /home/vagrant/.bash_profile
 echo 'export PATH=/home/vagrant/.pyenv/versions/3.7.2/bin:$PATH' >> /home/vagrant/.bash_profile && source /home/vagrant/.bash_profile
+
+# Install AWS SAM CLI
+pip install aws-sam-cli
+sam --version
